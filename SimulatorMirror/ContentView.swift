32f14a2257dea33c7a9961059d8c16ed608f4631
mendy-aouizerat <Mendy.aouizerat@shutterfly.com>
2026-03-01 11:06:58 +0200

    Initial Commit

---
+++ SimulatorMirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimulatorMirror
+//
+//  Created by mendy aouizerat on 01/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimulatorMirror/SimulatorMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorMirrorApp.swift
+//  SimulatorMirror
+//
+//  Created by mendy aouizerat on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimulatorMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
